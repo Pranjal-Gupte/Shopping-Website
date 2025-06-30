46fa198c6dc3705e2090bb7eae6806df3eb90532
Pranjal Gupte <pranjal55555@yahoo.com>
2025-06-30 21:10:51 +0530

    Added Ability to Add Brands and Fixed A Bug Where Preview Image Was 404 and Added New Package

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -16,5 +19,42 @@ class AdminController extends Controller
     {
         $brands = Brand::orderBy('id', 'desc')->paginate(10);
         return view('admin.brands', compact('brands'));
+    }
+
+    public function addBrand()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function storeBrand(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:brands,slug',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+        $this->GenerateBrandThumbnailsImage($image, $file_name);
+        $brand->image = $file_name;
+        $brand->save();
+
+        return redirect()->route('admin.brands')->with('status', 'Brand added successfully!');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/brands');
+        $img = Image::read($image->path());
+
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath. '/' . $imageName);
     }
 }
